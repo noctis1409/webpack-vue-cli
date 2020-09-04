5fb0fc79b603c27ab5a193e8c9286597bf43c2f9
orange <noctis1409@163.com>
2020-09-04 23:12:13 +0800

    animation

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,26 @@
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style scoped>
+@keyframes color {
+  0% {
+    color: #ff6666;
+  }
 
+  50% {
+    color: #66ccff;
+  }
+
+  100% {
+    color: #ff6666;
+  }
+}
+h1 {
+  text-align: center;
+  color: #ff6666;
+  animation: color 3s infinite linear;
+}
 </style>
